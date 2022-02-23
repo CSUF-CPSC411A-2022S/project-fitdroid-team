3ac01284024bdf10d35902bb26d6a337855347e4
ktranfullerton2000 <badsprite201x@gmail.com>
2022-02-23 15:37:01 -0800

    Update MainActivity.kt

--- app/src/main/java/com/example/fitdroid/MainActivity.kt
+++ app/src/main/java/com/example/fitdroid/MainActivity.kt
@@ -3,6 +3,19 @@ package com.example.fitdroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+class Food(var calories: Int, var name: String){
+    init{
+        print("making food")
+    }
+
+    constructor() : this(0, "unknown") {
+    }
+
+    fun printInfo(){
+        print("$name - calories:$calories")
+    }
+}
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
