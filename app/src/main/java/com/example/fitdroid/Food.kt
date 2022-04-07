c4e0394fa481cd44e2f906afef15f101ee42226d
ktranfullerton2000 <badsprite201x@gmail.com>
2022-04-07 13:01:45 -0700

    [WIP] adding recycle view & adding live data

--- app/src/main/java/com/example/fitdroid/Food.kt
+++ app/src/main/java/com/example/fitdroid/Food.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
 import com.example.fitdroid.databinding.FoodBinding
-
+import com.example.fitdroid.adapter.FoodListAdapter
+import com.google.android.material.internal.ContextUtils.getActivity
 
 class Food : Fragment() {
     override fun onCreateView(
@@ -19,7 +21,36 @@ class Food : Fragment() {
         binding.homeButton.setOnClickListener{ view: View ->
             view.findNavController().navigate(R.id.action_food_to_homepage)
         }
-
         return binding.root
     }
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        var binding = FoodBinding.inflate(layoutInflater)
+//        // root is a property holding the view of the inflated XML and is set as the contentView
+//        activity?.setContentView(binding.root)
+//
+//
+//        var foodListAdapter = FoodListAdapter(getActivity()?.this)
+//        binding.recyclerView.adapter = foodListAdapter
+//
+//
+//        binding.addFood.setOnClickListener {
+//
+//            val toast = Toast.makeText(
+//                this,
+//                "Adding ${binding.foodName.text} @ ${binding.foodAddress.text}",
+//                Toast.LENGTH_SHORT
+//            )
+//            toast.show()
+//
+//            // We can access the data through the dataset property inside foodListAdapter.the d
+//            foodListAdapter.dataset.add("${binding.foodName.text} @ ${binding.foodAddress.text}")
+//
+//            // Inform the adapter that we made changes so the visual representation can be updated.
+//            foodListAdapter.notifyDataSetChanged()
+//
+//        }
+//    }
 }
